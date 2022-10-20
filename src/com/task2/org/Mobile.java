edec0aff6160b8b53342d43b26210864baf674e1
jeniS2002 <ssagayaraj06@gmail.com>
2022-10-19 23:58:01 -0700

    Java

---
+++ src/com/task2/org/Mobile.java
@@ -0,0 +1,33 @@
+package com.task2.org;
+
+public class Mobile {
+
+	public void oppo() {
+
+		System.out.println("Name:Oppo A5s");
+	    System.out.println("Model:CPH1909");
+	    System.out.println("RAM:4.00 GB");
+	    System.out.println("Storage:8.06 GB");
+	    System.out.println("\n");
+	    }
+	
+	private void vivo() {
+		
+		System.out.println("Name:Vivo Y21");
+		System.out.println("Model:V2111");
+		System.out.println("RAM:4.00 GB");
+		System.out.println("Storage:64.00 GB");
+	}
+	
+	public static void main(String[] args) {
+	    Mobile objname=new Mobile();
+		Mobile obj = new Mobile();
+		obj.oppo();
+	
+		obj.vivo();
+	}
+}
+		
+
+
+
